0408f763ea4c85fbbab7abbf94ea0ea2ebe344b7
quangamexom <quangamexom@gmail.com>
2026-08-23 18:24:31 +0700

    feat(data): consolidate 150+ games default database & sync Vercel fallback

--- src/data/initialGames.ts
+++ src/data/initialGames.ts
@@ -1,8 +1,8 @@
 import { GameItem } from '../types';
-import googleSheetData from './googleSheetGames.json';
+import initialGamesData from './initialGames.json';
 
 export const DEFAULT_SHEET_URL = "https://docs.google.com/spreadsheets/d/1VA8Wv9OQmrR4nDpf0SUFQiqC4IAoVSCswCjY37ChplM/edit?gid=0#gid=0";
 export const DEFAULT_SHEET_ID = "1VA8Wv9OQmrR4nDpf0SUFQiqC4IAoVSCswCjY37ChplM";
 
-export const INITIAL_GAMES: GameItem[] = googleSheetData as GameItem[];
+export const INITIAL_GAMES: GameItem[] = initialGamesData as unknown as GameItem[];
 
