61efa6bebda912d23660c282b892f01cef7e6ac6
quangamexom <quangamexom@gmail.com>
2026-08-27 16:49:53 +0700

    chore(logo): update custom logo

--- src/data/customLogo.ts
+++ src/data/customLogo.ts
@@ -1,3 +1,3 @@
-export const OFFICIAL_LOGO_URL = 'https://cdn.discordapp.com/attachments/1477535897854410874/1541048383815548978/LOGO_QUAN_GAME_XOM.png?ex=6a8c2ca5&is=6a8adb25&hm=f99d6b1fed8028ce41620639309d03ae1feeecbe09ecb63ff5a52e0aa1a4291b&';
+export const OFFICIAL_LOGO_URL = 'https://cdn.discordapp.com/attachments/1477535897854410874/1541048383815548978/LOGO_QUAN_GAME_XOM.png?ex=6a90c9e5&is=6a8f7865&hm=a12da0249ce127e97c848b143998dd0c93b39561e920e987a9a8cd0387529356&';
 export const DEFAULT_LOGO_URL = '/assets/logo/logo-qgx-default.png';
-export const CUSTOM_LOGO_URL = 'https://cdn.discordapp.com/attachments/1477535897854410874/1541048383815548978/LOGO_QUAN_GAME_XOM.png?ex=6a8c2ca5&is=6a8adb25&hm=f99d6b1fed8028ce41620639309d03ae1feeecbe09ecb63ff5a52e0aa1a4291b&';
+export const CUSTOM_LOGO_URL = 'https://cdn.discordapp.com/attachments/1477535897854410874/1541048383815548978/LOGO_QUAN_GAME_XOM.png?ex=6a90c9e5&is=6a8f7865&hm=a12da0249ce127e97c848b143998dd0c93b39561e920e987a9a8cd0387529356&';
