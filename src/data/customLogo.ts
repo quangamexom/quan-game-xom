a89bf80829f3d6f55bf4943f209cdb4c7bb6b189
quangamexom <quangamexom@gmail.com>
2026-09-02 13:52:33 +0700

    Set /assets/logo/logo-qgx-default.png as primary official logo

--- src/data/customLogo.ts
+++ src/data/customLogo.ts
@@ -1,3 +1,3 @@
-export const OFFICIAL_LOGO_URL = 'https://cdn.discordapp.com/attachments/1477535897854410874/1541048383815548978/LOGO_QUAN_GAME_XOM.png?ex=6a90c9e5&is=6a8f7865&hm=a12da0249ce127e97c848b143998dd0c93b39561e920e987a9a8cd0387529356&';
+export const OFFICIAL_LOGO_URL = '/assets/logo/logo-qgx-default.png';
 export const DEFAULT_LOGO_URL = '/assets/logo/logo-qgx-default.png';
-export const CUSTOM_LOGO_URL = 'https://cdn.discordapp.com/attachments/1477535897854410874/1541048383815548978/LOGO_QUAN_GAME_XOM.png?ex=6a90c9e5&is=6a8f7865&hm=a12da0249ce127e97c848b143998dd0c93b39561e920e987a9a8cd0387529356&';
+export const CUSTOM_LOGO_URL = '/assets/logo/logo-qgx-default.png';
